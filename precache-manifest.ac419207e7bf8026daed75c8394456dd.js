4564cb25a69ec7b3638dd6fe0b7d66cc412079db
Sebastian Sossa <s_o_s_a_1999@hotmail.com>
2020-10-02 00:05:32 -0500

    Updates

---
+++ precache-manifest.ac419207e7bf8026daed75c8394456dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c208b03e16a82f16b7ad5f9ae674c0fc",
+    "url": "/mi-portafolio/index.html"
+  },
+  {
+    "revision": "b76e25c7508b34c3cd97",
+    "url": "/mi-portafolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed4364a1d8bda7031f31",
+    "url": "/mi-portafolio/static/js/2.fac01ade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mi-portafolio/static/js/2.fac01ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76e25c7508b34c3cd97",
+    "url": "/mi-portafolio/static/js/main.30a86237.chunk.js"
+  },
+  {
+    "revision": "086cf5de0b405ba726d8",
+    "url": "/mi-portafolio/static/js/runtime-main.65cbc2fc.js"
+  }
+]);
