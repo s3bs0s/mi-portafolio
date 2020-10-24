1b7cc25f9f753b59912762a90676d0b46a57be16
sebas <ssossa@insite.com.co>
2020-10-24 01:32:13 -0500

    LandingPage ya no tiene style.scss

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -3,7 +3,6 @@ import React, { Fragment } from 'react';
 import Seccion from '../Seccion';
 import Portada from './components/Portada';
 import base64 from '../../base64';
-import './style.scss';
 
 const LandingPage = () => {
   const tempString = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quos vero eligendi odio. Voluptate eaque soluta ducimus? Delectus architecto, sapiente veritatis dolores fugiat, fugit tempora magnam odit, officia illo veniam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, voluptatum soluta? Fuga, repellat optio. Quis quibusdam maxime molestias possimus autem deleniti earum sint similique, non sunt at iure officia mollitia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque laudantium beatae optio corrupti tempore earum recusandae.';
@@ -16,27 +15,27 @@ const LandingPage = () => {
       </Seccion>
 
       <Seccion ladosFigura='arriba-i abajo-i'>
-        <p className='text'>
-          {tempString + tempString}
+        <p style={{ fontSize: '19px' }}>
+          {tempString + tempString + tempString}
         </p>
       </Seccion>
       <Seccion ladosFigura='arriba-d abajo-d' tipoFondo='color'>
-        <p className='text'>
-          {tempString}
+        <p style={{ fontSize: '19px', color: '#fff' }}>
+          {tempString + tempString}
         </p>
       </Seccion>
       <Seccion ladosFigura='arriba-i abajo-i'>
-        <p className='text'>
+        <p style={{ fontSize: '19px' }}>
           {tempString}
         </p>
       </Seccion>
       <Seccion ladosFigura='arriba-d abajo-d' tipoFondo='color'>
-        <p className='text'>
-          {tempString + tempString}
+        <p style={{ fontSize: '19px', color: '#fff' }}>
+          {tempString + tempString + tempString}
         </p>
       </Seccion>
       <Seccion ladosFigura='arriba-i'>
-        <p className='text'>
+        <p style={{ fontSize: '19px' }}>
           {tempString}
         </p>
       </Seccion>
