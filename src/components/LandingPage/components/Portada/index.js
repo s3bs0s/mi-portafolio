5e72a43e34457a5dd5a6e84144a12a8170b5416b
Sebastian Sossa <s_o_s_a_1999@hotmail.com>
2020-10-24 21:27:36 -0500

    Nuevo sub título de imagen sin barra de escribir

--- src/components/LandingPage/components/Portada/index.js
+++ src/components/LandingPage/components/Portada/index.js
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import base64 from '../../../../base64';
 import './style.scss';
 
 const Portada = () => {
-  const [ getBarraEscribir, setBarraEscribir ] = useState('transparent');
-
-  setTimeout(() => setBarraEscribir(getBarraEscribir === 'transparent' ? '#bdbdbd' : 'transparent'), 900)
-
   return (
     <div className='portada'>
       <div className='logo'>
         <img src={base64.logoPng} alt='Logo'/>
       </div>
       <div className='titulos'>
-        <img src={base64.tituloPng} alt='Título'/>
-        <div className='sub-titulo'>
-          <h1>Sebastian Sossa</h1>
-          <span className='barra-escribir' style={{ background: getBarraEscribir }}/>
-        </div>
+        <img src={base64.tituloPng} className='titulo' alt='Título'/>
+        <img src={base64.subTituloPng} className='sub-titulo' alt='Sub Título'/>
       </div>
     </div>
   );
