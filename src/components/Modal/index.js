962a1720bca2c7d718a47fba68118a291f1aee87
Sebastian Sossa <s_o_s_a_1999@hotmail.com>
2020-10-24 19:59:51 -0500

    Los archivos de los futuros componentes globales fueron inicializados

---
+++ src/components/Modal/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.scss';
+
+const Modal = () => {
+  return <p className='modal'/>;
+}
+
+export default Modal;
---
+++ src/components/ScrollBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.scss';
+
+const ScrollBar = () => {
+  return <p className='scroll-bar'/>;
+}
+
+export default ScrollBar;
